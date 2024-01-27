0246f6be98437e3aeea5dc345f26db9fdfd9c126
ikoamu <ikoamu@gmail.com>
2024-01-27 16:55:45 +0900

    Refactor create_notes.sh script to copy files based on graphdata.json

--- create_notes.sh
+++ create_notes.sh
@@ -4,14 +4,19 @@ orgPath=$1
 
 mkdir -p notes
 
-cat graphdata.json
+# cat graphdata.json
 
-ls -ls "${orgPath}"
+# ls -ls "${orgPath}"
 
 cat graphdata.json |
 jq -c '.data.nodes[]' |
 while read -r nodes; do
   id=$(echo "${nodes}" | jq -r '.id')
   file=$(echo "${nodes}" | jq -r '.file')
+  echo "============================="
+  echo "id: ${id}"
+  echo "file: ${file}"
+  cat "${orgPath}/${file}"
+
   cp -p "${orgPath}/${file}" "notes/${id}"
 done
