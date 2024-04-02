f70144d438a7d7a548b02e41a514ccbd1792c4aa
ikoamu <ikoamu@gmail.com>
2024-04-02 17:53:48 +0900

    Remove unnecessary echo

--- create_notes.sh
+++ create_notes.sh
@@ -8,9 +8,7 @@ cat graphdata.json |
 jq -c '.data.nodes[]' |
 while read -r nodes; do
   id=$(echo "${nodes}" | jq -r '.id')
-  file=$(echo "${nodes}" | jq -r '.file')
-  echo "============================="
-  
+  file=$(echo "${nodes}" | jq -r '.file') 
   timestamp=$(echo "${file}" | cut -d'-' -f1)
   matching_file=$(find "${org_path}" -name "${timestamp}-*.org" -type f)
   
