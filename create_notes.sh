ae44476b8faea9a5a098d3f8c91b8908e9cef508
ikoamu <ikoamu@gmail.com>
2024-04-04 08:48:27 +0900

    support no-timestamp file

--- create_notes.sh
+++ create_notes.sh
@@ -9,8 +9,7 @@ jq -c '.data.nodes[]' |
 while read -r nodes; do
   id=$(echo "${nodes}" | jq -r '.id')
   file=$(echo "${nodes}" | jq -r '.file') 
-  timestamp=$(echo "${file}" | cut -d'-' -f1)
-  matching_file=$(find "${org_path}" -name "${timestamp}-*.org" -type f)
+  matching_file=$(find "${org_path}" -name "${file}" -type f)
   
   if [[ -f "${matching_file}" ]]; then
     cp -p "${matching_file}" "notes/${id}"
